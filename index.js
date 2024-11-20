6a826a7aaec7b228df81578f2fc906fe4b90b456
kajedevid <stokakunnnnn23@gmail.com>
2024-11-20 08:31:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-global.adUrl = ""
+global.adUrl = "https://gist.githubusercontent.com/kazedepid/c20b4355e58044eb0cde5b5f232ff4a1/raw/8b0c8d6bd51a30b26fd7e3b35ec72747d34370c5/adsku.json"
 
 module.exports = {
   downloaders: require('./lib/downloader.js'),
