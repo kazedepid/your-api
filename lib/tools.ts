32f6a18f956ce0f5d7586400157b0ec3ab6afb3b
kajedevid <stokakunnnnn23@gmail.com>
2024-11-25 11:43:49 +0800

    Add files via upload

---
+++ lib/tools.ts
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import cheerio from 'cheerio'
+
+async function ttp(text: string): Promise<string[]> {
+    try {
+        const { data } = await axios.post("https://www.picturetopeople.org/p2p/text_effects_generator.p2p/transparent_text_effect", new URLSearchParams({
+            TextToRender: text,
+            FontSize: "100",
+            Margin: "30",
+            LayoutStyle: "0",
+            TextRotation: "0",
+            TextColor: "ffffff",
+            TextTransparency: "0",
+            OutlineThickness: "3",
+            OutlineColor: "000000",
+            FontName: "Lekton",
+            ResultType: "view",
+        }).toString())
+
+        const $ = cheerio.load(data)
+        return $('form[name="MyForm"] #idResultFile').map((_, el) => 'https://www.picturetopeople.org' + $(el).val()).get()
+    } catch (error) {
+        console.error('Error:', error)
+        return []
+    }
+}
+
+async function shortenUrl(url: string): Promise<string | null> {
+    try {
+        const { data } = await axios.post("https://www.shorturl.at/shortener.php", new URLSearchParams({ u: url }))
+        return data.split('value="')[1].split('"')[0]
+    } catch (error) {
+        console.error(error)
+        return null
+    }
+}
+
+export { ttp, shortenUrl }
